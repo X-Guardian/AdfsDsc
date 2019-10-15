6762ebad6549948f4480dbf416350f3317350d74
Simon Heather <simonheather99@gmail.com>
2019-10-15 22:07:13 +0100

    New resource AdfsContactPerson

---
+++ DSCResources/MSFT_AdfsContactPerson/MSFT_AdfsContactPerson.psm1
@@ -0,0 +1,250 @@
+<#
+    .SYNOPSIS
+        DSC module for the ADFS Contact Person resource
+
+    .DESCRIPTION
+        The AdfsContactPerson DSC resource manages the ADFS contact information for support isssues.
+
+    .PARAMETER FederationServiceName
+        Key - String
+        Specifies the DNS name of the federation service.
+
+    .PARAMETER Company
+        Key - String
+        Specifies the company name of the contact person.
+
+    .PARAMETER EmailAddress
+        Key - String[]
+        Specifies an array of e-mail addresses of the contact person.
+
+    .PARAMETER GivenName
+        Key - String
+        Specifies the given name, or first name of the contact person.
+
+    .PARAMETER Surname
+        Key - String
+        Specifies the surname, or last name of the contact person.
+
+    .PARAMETER TelephoneNumber
+        Key - String[]
+        Specifies an array of telephone numbers of the contact person.
+#>
+
+Set-StrictMode -Version Latest
+
+$script:dscModuleName = 'AdfsDsc'
+$script:PSModuleName = 'ADFS'
+$script:dscResourceName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
+$script:resourceModulePath = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+$script:modulesFolderPath = Join-Path -Path $script:resourceModulePath -ChildPath 'Modules'
+
+$script:localizationModulePath = Join-Path -Path $script:modulesFolderPath -ChildPath "$($script:DSCModuleName).Common"
+Import-Module -Name (Join-Path -Path $script:localizationModulePath -ChildPath "$($script:dscModuleName).Common.psm1")
+
+$script:localizedData = Get-LocalizedData -ResourceName $script:dscResourceName
+
+function Get-TargetResource
+{
+    <#
+    .SYNOPSIS
+        Get-TargetResource
+
+    .NOTES
+        Used Resource PowerShell Cmdlets:
+        - Get-AdfsProperties - https://docs.microsoft.com/en-us/powershell/module/adfs/get-adfsproperties
+    #>
+
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $FederationServiceName
+    )
+
+    # Check of the ADFS PowerShell module is installed
+    Assert-Module -ModuleName $script:PSModuleName
+
+    # Check if the ADFS Service is present and running
+    Assert-AdfsService -Verbose
+
+    Write-Verbose -Message ($script:localizedData.GettingResourceMessage -f $FederationServiceName)
+
+    try
+    {
+        $targetResource = (Get-AdfsProperties).ContactPerson
+    }
+    catch
+    {
+        $errorMessage = $script:localizedData.GettingResourceError -f $FederationServiceName
+        New-InvalidOperationException -Message $errorMessage -Error $_
+    }
+
+    $returnValue = @{
+        FederationServiceName = $FederationServiceName
+        Company               = $targetResource.Company
+        EmailAddress          = $targetResource.EmailAddress
+        GivenName             = $targetResource.GivenName
+        Surname               = $targetResource.Surname
+        TelephoneNumber       = $targetResource.TelephoneNumber
+    }
+
+    $returnValue
+}
+
+
+function Set-TargetResource
+{
+    <#
+    .SYNOPSIS
+        Set-TargetResource
+
+    .NOTES
+        Used Resource PowerShell Cmdlets:
+        - New-AdfsContactPerson -
+        - Set-AdfsProperties - https://docs.microsoft.com/en-us/powershell/module/adfs/set-adfsproperties
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $FederationServiceName,
+
+        [Parameter()]
+        [System.String]
+        $Company,
+
+        [Parameter()]
+        [System.String[]]
+        $EmailAddress,
+
+        [Parameter()]
+        [System.String]
+        $GivenName,
+
+        [Parameter()]
+        [System.String]
+        $Surname,
+
+        [Parameter()]
+        [System.String[]]
+        $TelephoneNumber
+    )
+
+    # Remove any parameters not used in Splats
+    [HashTable]$parameters = $PSBoundParameters
+    $parameters.Remove('FederationServiceName')
+    $parameters.Remove('Verbose')
+
+    $GetTargetResourceParms = @{
+        FederationServiceName = $FederationServiceName
+    }
+    $targetResource = Get-TargetResource @GetTargetResourceParms
+
+    $propertiesNotInDesiredState = (
+        Compare-ResourcePropertyState -CurrentValues $targetResource -DesiredValues $parameters |
+            Where-Object -Property InDesiredState -eq $false)
+
+    foreach ($property in $propertiesNotInDesiredState)
+    {
+        Write-Verbose -Message (
+            $script:localizedData.SettingResourceMessage -f
+            $FederationServiceName, $property.ParameterName, ($property.Expected -join ', '))
+    }
+
+    try {
+        $contactPerson = New-AdfsContactPerson @parameters
+    }
+    catch {
+        $errorMessage = $script:localizedData.NewAdfsContactPersonError -f $FederationServiceName
+        New-InvalidOperationException -Message $errorMessage -Error $_
+    }
+    try
+    {
+        Set-AdfsProperties -ContactPerson $contactPerson
+    }
+    catch
+    {
+        $errorMessage = $script:localizedData.SettingResourceError -f $FederationServiceName
+        New-InvalidOperationException -Message $errorMessage -Error $_
+    }
+}
+
+
+function Test-TargetResource
+{
+    <#
+    .SYNOPSIS
+        Test-TargetResource
+    #>
+
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $FederationServiceName,
+
+        [Parameter()]
+        [System.String]
+        $Company,
+
+        [Parameter()]
+        [System.String[]]
+        $EmailAddress,
+
+        [Parameter()]
+        [System.String]
+        $GivenName,
+
+        [Parameter()]
+        [System.String]
+        $Surname,
+
+        [Parameter()]
+        [System.String[]]
+        $TelephoneNumber
+    )
+
+    [HashTable]$parameters = $PSBoundParameters
+    $parameters.Remove('FederationServiceName')
+
+    $GetTargetResourceParms = @{
+        FederationServiceName = $FederationServiceName
+    }
+    $targetResource = Get-TargetResource @GetTargetResourceParms
+
+    $propertiesNotInDesiredState = (
+        Compare-ResourcePropertyState -CurrentValues $targetResource -DesiredValues $parameters |
+            Where-Object -Property InDesiredState -eq $false)
+
+    if ($propertiesNotInDesiredState)
+    {
+        # Resource is not in desired state
+        foreach ($property in $propertiesNotInDesiredState)
+        {
+            Write-Verbose -Message (
+                $script:localizedData.ResourcePropertyNotInDesiredStateMessage -f
+                $targetResource.FederationServiceName, $property.ParameterName, `
+                    $property.Expected, $property.Actual)
+        }
+        $inDesiredState = $false
+    }
+    else
+    {
+        # Resource is in desired state
+        Write-Verbose -Message ($script:localizedData.ResourceInDesiredStateMessage -f $FederationServiceName)
+        $inDesiredState = $true
+    }
+
+    $inDesiredState
+}
+
+
+Export-ModuleMember -Function *-TargetResource
+
---
+++ Tests/Unit/MSFT_AdfsContactPerson.Tests.ps1
@@ -0,0 +1,219 @@
+$Global:DSCModuleName = 'AdfsDsc'
+$Global:PSModuleName = 'ADFS'
+$Global:DSCResourceName = 'MSFT_AdfsContactPerson'
+
+$moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
+if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
+    (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
+{
+    & git @('clone', 'https://github.com/PowerShell/DscResource.Tests.git',
+        (Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'))
+}
+
+Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
+
+$TestEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $Global:DSCModuleName `
+    -DSCResourceName $Global:DSCResourceName `
+    -TestType Unit
+
+try
+{
+    InModuleScope $Global:DSCResourceName {
+        # Import ADFS Stub Module
+        Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "Stubs\$($Global:PSModuleName)Stub.psm1") -Force
+
+        # Define Resource Commands
+        $ResourceCommand = @{
+            Get = 'Get-AdfsProperties'
+            Set = 'Set-AdfsProperties'
+        }
+
+        $mockResource = @{
+            FederationServiceName = 'sts.contoso.com'
+            Company               = 'Contoso'
+            EmailAddress          = 'support@contoso.com'
+            GivenName             = 'Bob'
+            Surname               = 'Smith'
+            TelephoneNumber       = '+1 555 12345678'
+        }
+
+        $mockChangedResource = @{
+            Company         = 'Fabrikam'
+            EmailAddress    = 'support@fabrikam.com'
+            GivenName       = 'Fred'
+            Surname         = 'Blogs'
+            TelephoneNumber = '+1 555 87654321'
+        }
+
+        $mockGetTargetResourceResult = @{
+            FederationServiceName = $mockResource.FederationServiceName
+            Company               = $mockResource.Company
+            EmailAddress          = $mockResource.EmailAddress
+            GivenName             = $mockResource.GivenName
+            Surname               = $mockResource.Surname
+            TelephoneNumber       = $mockResource.TelephoneNumber
+        }
+
+        Describe "$Global:DSCResourceName\Get-TargetResource" -Tag 'Get' {
+            BeforeAll {
+                $getTargetResourceParameters = @{
+                    FederationServiceName = $mockResource.FederationServiceName
+                }
+
+                $mockGetResourceCommandResult = @{
+                    ContactPerson = @{
+                        Company         = $mockResource.Company
+                        EmailAddress    = $mockResource.EmailAddress
+                        GivenName       = $mockResource.GivenName
+                        Surname         = $mockResource.Surname
+                        TelephoneNumber = $mockResource.TelephoneNumber
+                    }
+                }
+
+                Mock -CommandName Assert-Module
+                Mock -CommandName "Assert-$($Global:PSModuleName)Service"
+                Mock -CommandName $ResourceCommand.Get -MockWith { $mockGetResourceCommandResult }
+
+                $result = Get-TargetResource @getTargetResourceParameters
+            }
+
+            foreach ($property in $mockResource.Keys)
+            {
+                It "Should return the correct $property property" {
+                    $result.$property | Should -Be $mockResource.$property
+                }
+            }
+
+            It 'Should call the expected mocks' {
+                Assert-MockCalled -CommandName Assert-Module `
+                    -ParameterFilter { $ModuleName -eq $Global:PSModuleName } `
+                    -Exactly -Times 1
+                Assert-MockCalled -CommandName "Assert-$($Global:PSModuleName)Service" -Exactly -Times 1
+                Assert-MockCalled -CommandName $ResourceCommand.Get -Exactly -Times 1
+            }
+
+            Context "When $($ResourceCommand.Get) throws an exception" {
+                BeforeAll {
+                    Mock -CommandName $ResourceCommand.Get -MockWith { Throw 'Error' }
+                }
+
+                It 'Should throw the correct exception' {
+                    { Get-TargetResource @getTargetResourceParameters } | Should -Throw (
+                        $script:localizedData.GettingResourceError -f $getTargetResourceParameters.FederationServiceName )
+                }
+            }
+        }
+
+        Describe "$Global:DSCResourceName\Set-TargetResource" -Tag 'Set' {
+            BeforeAll {
+                $setTargetResourceParameters = @{
+                    FederationServiceName = $mockResource.FederationServiceName
+                    Company               = $mockResource.Company
+                    EmailAddress          = $mockResource.EmailAddress
+                    GivenName             = $mockResource.GivenName
+                    Surname               = $mockResource.Surname
+                    TelephoneNumber       = $mockResource.TelephoneNumber
+                }
+
+                Mock -CommandName $ResourceCommand.Set
+                Mock -CommandName New-AdfsContactPerson -MockWith { New-MockObject -Type Microsoft.IdentityServer.Management.Resources.ContactPerson }
+                Mock -CommandName Get-TargetResource -MockWith { $mockGetTargetResourceResult }
+            }
+
+            foreach ($property in $mockChangedResource.Keys)
+            {
+                Context "When $property has changed" {
+                    BeforeAll {
+                        $setTargetResourceParametersChangedProperty = $setTargetResourceParameters.Clone()
+                        $setTargetResourceParametersChangedProperty.$property = $mockChangedResource.$property
+                    }
+
+                    It 'Should not throw' {
+                        { Set-TargetResource @setTargetResourceParametersChangedProperty } | Should -Not -Throw
+                    }
+
+                    It 'Should call the correct mocks' {
+                        Assert-MockCalled -CommandName Get-TargetResource `
+                            -ParameterFilter { `
+                                $FederationServiceName -eq $setTargetResourceParametersChangedProperty.FederationServiceName } `
+                            -Exactly -Times 1
+                        Assert-MockCalled -CommandName $ResourceCommand.Set -Exactly -Times 1
+                        Assert-MockCalled -CommandName New-AdfsContactPerson -Exactly -Times 1
+                    }
+                }
+            }
+
+            Context 'When New-AdfsContactPerson throws an exception' {
+                BeforeAll {
+                    Mock -CommandName New-AdfsContactPerson -MockWith { Throw 'Error' }
+                }
+
+                It 'Should throw the correct exception' {
+                    { Set-TargetResource @setTargetResourceParameters } | Should -Throw (
+                        $script:localizedData.NewAdfsContactPersonError -f $setTargetResourceParameters.FederationServiceName )
+                }
+            }
+
+            Context "When $($ResourceCommand.Set) throws an exception" {
+                BeforeAll {
+                    Mock -CommandName $ResourceCommand.Set -MockWith { Throw 'Error' }
+                }
+
+                It 'Should throw the correct exception' {
+                    { Set-TargetResource @setTargetResourceParameters } | Should -Throw (
+                        $script:localizedData.SettingResourceError -f $setTargetResourceParameters.FederationServiceName )
+                }
+            }
+        }
+
+        Describe "$Global:DSCResourceName\Test-TargetResource" -Tag 'Test' {
+            BeforeAll {
+                $testTargetResourceParameters = @{
+                    FederationServiceName = $mockResource.FederationServiceName
+                    Company               = $mockResource.Company
+                    EmailAddress          = $mockResource.EmailAddress
+                    GivenName             = $mockResource.GivenName
+                    Surname               = $mockResource.Surname
+                    TelephoneNumber       = $mockResource.TelephoneNumber
+                }
+
+                Mock -CommandName Get-TargetResource -MockWith { $mockGetTargetResourceResult }
+            }
+
+            It 'Should not throw' {
+                { Test-TargetResource @testTargetResourceParameters } | Should -Not -Throw
+            }
+
+            It 'Should call the expected mocks' {
+                Assert-MockCalled -CommandName Get-TargetResource `
+                    -ParameterFilter { $FederationServiceName -eq $testTargetResourceParameters.FederationServiceName } `
+                    -Exactly -Times 1
+            }
+
+            Context 'When all the resource properties are in the desired state' {
+                It 'Should return $true' {
+                    Test-TargetResource @testTargetResourceParameters | Should -Be $true
+                }
+            }
+
+            foreach ($property in $mockChangedResource.Keys)
+            {
+                Context "When the $property resource property is not in the desired state" {
+                    BeforeAll {
+                        $testTargetResourceNotInDesiredStateParameters = $testTargetResourceParameters.Clone()
+                        $testTargetResourceNotInDesiredStateParameters.$property = $mockChangedResource.$property
+                    }
+
+                    It 'Should return $false' {
+                        Test-TargetResource @testTargetResourceNotInDesiredStateParameters | Should -Be $false
+                    }
+                }
+            }
+        }
+    }
+}
+finally
+{
+    Restore-TestEnvironment -TestEnvironment $TestEnvironment
+}
